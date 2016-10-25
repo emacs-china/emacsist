9eb2cb617447fd4990e5214b532c4aedcf0429b1
guobao.jiang <loveaborn@foxmail.com>
2016-10-25 09:40:25 +0800

    add emacsist.el

---
+++ emacsist.el
@@ -0,0 +1,12 @@
+;;; emacsist.el --- produce REAEME.org  -*- lexical-binding: t; -*-
+
+;;
+
+;;; Code:
+
+(defun emacsist ()
+  (interactive)
+  (message "finished."))
+
+(provide 'emacsist)
+;;; emacsist.el ends here
