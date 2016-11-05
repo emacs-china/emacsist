6c811b12786654380cd5e021f5baffb5a111c234
Aborn Jiang <loveaborn@foxmail.com>
2016-11-05 10:05:40 +0800

    完成emacsist

--- emacsist.el
+++ emacsist.el
@@ -4,9 +4,48 @@
 
 ;;; Code:
 
+(require 'f)
+
+(defgroup emacsist nil
+  "leanote mini group"
+  :prefix "emacsist-"
+  :group 'org)
+
+(defcustom emacsist-repo-root "~/github/emacsist"
+  "the emacsist repo root path"
+  :group 'emacsist
+  :type 'string)
+
 (defun emacsist ()
   (interactive)
-  (message "finished."))
+  (emacsist-append-articles-list)
+  (message "生成文章列表成功!"))
+
+(defun emacsist-articles-list-content ()
+  (let* ((apath (expand-file-name "articles" emacsist-repo-root)))
+    (mapcar
+     #'(lambda (item)
+         (format "[[./articles/%s][%s]]\n"
+                 (f-filename item)
+                 (f-base item)))
+     (f-files apath
+              (lambda (file) (f-ext? file "org"))))
+    ))
+
+(defun emacsist-append-articles-list ()
+  "Find the correct position"
+  (let* ((fname (expand-file-name "README.org" emacsist-repo-root)))
+    (find-file fname)
+    (goto-char (point-min))
+    (let ((spos (search-forward "** 往期文章")))
+      (when spos
+        (kill-region spos (point-max))
+        (insert "\n")
+        (mapc #'(lambda (item)
+                  (insert item))
+              (emacsist-articles-list-content))
+        (save-buffer)
+        ))))
 
 (provide 'emacsist)
 ;;; emacsist.el ends here
