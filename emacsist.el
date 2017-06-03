bd2d4374eb33706d09a649265c7bfcbcf718a40e
guobao.jiang <guobao.jiang@dianping.com>
2017-06-03 15:41:20 +0800

    (magit-stage-file)

--- emacsist.el
+++ emacsist.el
@@ -97,6 +97,7 @@
           (f-move fname des-name)
           (kill-buffer)
           (find-file des-name)
+          (magit-stage-file)
           (message "原稿件:%s, 已经发布到:%s" fname des-name)
           (emacsist))      ;; 生成目录
       (message "当前文件 %s 不在目录tougao/下，不能发布！" fname))))
